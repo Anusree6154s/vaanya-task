19c2f141139340728c15dd27d19db403f0447783
Anusree <anilkumaranusree113@gmail.com>
2025-01-28 18:05:23 +0530

    edited frontend url

--- frontend/src/utils/contants.js
+++ frontend/src/utils/contants.js
@@ -1,2 +1,2 @@
-export const backendURL = "http://localhost:8080"
-export const backendURLHosted = "https://vaanya-task-backend.onrender.com"
+// export const backendURL = "http://localhost:8080"
+export const backendURL = "https://vaanya-task-backend.onrender.com"
